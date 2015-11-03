9eb65a2d0f821e624801632671e9c8b010939932
fy <fy0@qq.com>
2015-11-03 18:54:31 +0800

    fix

--- view/substitution.py
+++ view/substitution.py
@@ -5,6 +5,7 @@ import zlib
 import base64
 from view import route, url_for, View, LoginView, AjaxLoginView, AjaxView
 
+
 @route('/j/subsitution', name='j_substitution')
 class OneLine(AjaxLoginView):
     def post(self):
